9733625f0c0075f9fb3f274eb4c5e436f264cef4
Iftekhar Utsho <future.slangsta76@gmail.com>
2022-04-13 04:05:41 +0600

    responsive nav added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
 import Home from './Pages/Home/Home/Home';
+import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 
@@ -12,6 +13,7 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/service/:serviceId' element={<ServiceDetail></ServiceDetail>}></Route>
         <Route path='/about' element={<About></About>}></Route>
       </Routes>
       <Footer></Footer>
--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Service.css'
 
 const Service = ({service}) => {
-    const {name, price, description, img} = service;
+    const {name, price, description, img, id} = service;
+    const navigate = useNavigate();
+    const handleService = (id) => {
+        navigate(`/service/${id}`)
+    }
     return (
         <div className='service'>
-            <img src={img} alt="" />
+            <img className='rounded-3' src={img} alt="" />
             <h2>{name}</h2>
             <h4>${price}</h4>
             <p>{description}</p>
-            <button>Book: {name}</button>
+            <button onClick={() => handleService(id)}>Book: {name}</button>
         </div>
     );
 };
